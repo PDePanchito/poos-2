63c99a39123f8ea82338da4296e69b97c6749323
Francisco Martínez <franciscomqbn@gmail.com>
2025-12-07 20:53:01 -0300

    feat: improve economic controller method to include optional date parameter for data retrieval

--- controller/economic_index_controller.py
+++ controller/economic_index_controller.py
@@ -7,16 +7,18 @@ class EconomicIndexController:
     def __init__(self, db: Database):
         self.db = db
 
-    def fetch_economic_data(self, username):
+    def fetch_economic_data(self, username, date=None):
         print("\nÍndices Económicos Disponibles:")
         self._economic_index_menu()
         index = Security.clean_text(input("Ingrese el nombre del índice económico que desea consultar: ")).lower()
         to_save = Security.clean_text(input("¿Desea guardar los datos en la base de datos? (s/n): ")).lower()
         to_save = True if to_save == "s" else False
         economic_service = EconomicService()
-        economic_data = economic_service.fetch_and_save(username, self.db, index, to_save)
-        if economic_data == "success":
-            print("Operación completada con éxito.")
+        economic_data = economic_service.fetch_and_save(username, self.db, index, to_save, date)
+        if economic_data == "error":
+            print("No se pudo completar la operación.")
+            return
+        print("Operación completada con éxito.")
 
     def _economic_index_menu(self):
         indexes = self._get_available_indexes()
